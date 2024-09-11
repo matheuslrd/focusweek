59fc11d41958cec3d6043f25683a4f35857156e1
Matheus Laurindo <matheus104521@gmail.com>
2024-09-11 19:56:36 -0300

    create goal post

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,32 @@
 import fastify from 'fastify';
+import zod from 'zod';
+import { createGoal } from '../functions/create-goal';
+import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from 'fastify-type-provider-zod';
 
-const app = fastify();
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.post(
+    '/goals',
+    {
+        schema: {
+            body: zod.object({
+                title: zod.string(),
+                desiredWeeklyFrequency: zod.number().int().min(1).max(7),
+            }),
+        },
+    },
+    async (req, res) => {
+        const { desiredWeeklyFrequency, title } = req.body;
+
+        const { goal } = await createGoal({
+            desiredWeeklyFrequency,
+            title,
+        });
+    },
+);
 
 app.listen({
     port: 3333,
